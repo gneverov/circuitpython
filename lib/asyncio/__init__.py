58898013332c2dcf9ef50c8ba6cab4c6744aa0ae
Gregory Neverov <42853258+gneverov@users.noreply.github.com>
2023-02-09 15:07:52 -0800

    import copy of CPython's asyncio implementation

---
+++ lib/asyncio/__init__.py
@@ -0,0 +1,41 @@
+"""The asyncio package, tracking PEP 3156."""
+
+# This relies on each of the submodules having an __all__ variable.
+from .base_events import *
+from .coroutines import *
+from .events import *
+from .exceptions import *
+from .futures import *
+# from .locks import *
+# from .protocols import *
+# from .runners import *
+# from .queues import *
+# from .streams import *
+# from .subprocess import *
+from .tasks import *
+# from .taskgroups import *
+# from .timeouts import *
+# from .threads import *
+# from .transports import *
+
+__all__ = (
+    base_events.__all__ +
+    coroutines.__all__ +
+    events.__all__ +
+    exceptions.__all__ +
+    futures.__all__ +
+    # locks.__all__ +
+    # protocols.__all__ +
+    # runners.__all__ +
+    # queues.__all__ +
+    # streams.__all__ +
+    # subprocess.__all__ +
+    tasks.__all__ +
+    # threads.__all__ +
+    # timeouts.__all__ +
+    # transports.__all__ +
+    ()
+)
+
+from .cp_events import *
+__all__ += cp_events.__all__
